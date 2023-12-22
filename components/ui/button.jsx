a1d5113aee3f865e2c1dbc31e57714e4711f3083
wgd753 <thor753@foxmail.com>
2023-12-22 16:45:04 +0800

    update

--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -13,11 +13,12 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground border-gray-700",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        dark: "bg-black text-white", // 新增的变体
       },
       size: {
         default: "h-10 px-4 py-2",
--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils"
 const Card = React.forwardRef(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
+    className={cn("rounded-lg border bg-white text-card-foreground shadow-sm", className)}
     {...props} />
 ))
 Card.displayName = "Card"
