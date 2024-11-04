d37da7009756e68cc403e8de3be558ebd170ce63
wgd753 <thor753@foxmail.com>
2024-11-04 17:19:35 +0800

    uodate

--- pages/blog/posts/[id].js
+++ pages/blog/posts/[id].js
@@ -278,12 +278,32 @@ export default function BlogPost({ post }) {
       </Head>
 
       <main className="container mx-auto px-4 py-8">
+        <nav className="text-sm mb-8" aria-label="Breadcrumb">
+          <ol className="list-none p-0 inline-flex">
+            <li className="flex items-center">
+              <Link href="/">
+                <span className="text-indigo-600 hover:text-indigo-800">Home</span>
+              </Link>
+              <svg className="fill-current w-3 h-3 mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
+              </svg>
+            </li>
+            <li className="flex items-center">
+              <Link href="/blog">
+                <span className="text-indigo-600 hover:text-indigo-800">Blog</span>
+              </Link>
+              <svg className="fill-current w-3 h-3 mx-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
+              </svg>
+            </li>
+            <li>
+              <span className="text-gray-500">{post.title}</span>
+            </li>
+          </ol>
+        </nav>
+
         <article className="max-w-3xl mx-auto prose prose-lg">
-          <Link href="/blog">
-            <span className="text-indigo-600 hover:text-indigo-800">← Back to Blog</span>
-          </Link>
-          
-          <header className="text-center my-12">
+          <header className="text-center mb-12">
             <div className="text-6xl mb-4">{post.emoji}</div>
             <div className="text-sm text-indigo-500 font-semibold mb-2">
               {post.category}
