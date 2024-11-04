e469c929e8c3b10095f1d22881b58b5b8f1fd31a
wgd753 <thor753@foxmail.com>
2024-11-04 14:46:08 +0800

    update

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,10 +2,8 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh', 'ja', 'ko', 'es'],
-    localeDetection: false,
+    localeDetection: true,
   },
   defaultNS: 'common',
   localePath: './public/locales',
-  reloadOnPrerender: true,
-  debug: process.env.NODE_ENV === 'development',
 } 
